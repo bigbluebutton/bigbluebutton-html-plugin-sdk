c992576e96737117fd713363d4e5a6d0e5e370bb
Tiago Jacobs <tiago.jacobs@gmail.com>
2023-11-30 11:44:49 -0300

    Modify getJoinUrl to use object instead of map

--- src/core/auxiliar/join-url/getter.ts
+++ src/core/auxiliar/join-url/getter.ts
@@ -1,13 +1,13 @@
 import { getSessionToken } from '../session-token/getter';
 
-function objectToUrlParameters(parameters: Map<string, string>): string {
+function objectToUrlParameters(parameters: {[key: string]: string}): string {
   const queryString = Object.entries(parameters)
     .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
     .join('&');
   return queryString;
 }
 
-export async function getJoinUrl(parameters: Map<string, string>): Promise<string> {
+export async function getJoinUrl(parameters: {[key: string]: string}): Promise<string> {
   const urlParameters = objectToUrlParameters(parameters);
   const url = `${document.location.origin}/bigbluebutton/api/getJoinUrl?sessionToken=${getSessionToken()}&${urlParameters}`;
   const response = await fetch(url);
