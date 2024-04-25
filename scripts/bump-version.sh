dd7228f5c181df6466776aba7e4d02d819443dbc
Tiago Jacobs <tiago.jacobs@gmail.com>
2024-04-25 15:13:36 -0300

    Add push to bump

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -53,4 +53,4 @@ git add package-lock.json package.json samples/*/package-lock.json samples/*/pac
 git commit -m "Bump version to $NEW_VERSION"
 git tag v$NEW_VERSION
 git push origin v$NEW_VERSION
-
+git push
