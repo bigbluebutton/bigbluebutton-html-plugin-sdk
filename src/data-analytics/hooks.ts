f6bd330b1908c3d3441de039f7162ba582fe84ee
Guilherme Leme <leme.guilherme.p@gmail.com>
2024-07-04 11:12:18 -0300

    [option-and-analytics] - quick fix

--- src/data-analytics/hooks.ts
+++ src/data-analytics/hooks.ts
@@ -6,7 +6,8 @@ import { DataAnalyticsSendObjectEvents } from './enums';
 
 export const sendDataAnalytics = (dataAnalyticsObject: DataAnalyticsObject, pluginName: string) => {
   window.dispatchEvent(
-    new CustomEvent<DataAnalyticsEventDetails>(DataAnalyticsSendObjectEvents.PILED_UP_OBJECT, {
+    new CustomEvent<
+    DataAnalyticsEventDetails>(DataAnalyticsSendObjectEvents.SEND_DATA_ANALYTICS_OBJECT, {
       detail: {
         pluginName,
         dataAnalyticsObject,
