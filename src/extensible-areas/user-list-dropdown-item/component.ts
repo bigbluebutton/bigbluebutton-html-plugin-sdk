48f356e468c067bda87ed265ebb1ea9e54abb050
Guilherme Pereira Leme <69865537+GuiLeme@users.noreply.github.com>
2024-09-30 14:05:30 -0300

    feat(html5) - UserListDropdownItems refactor (#116)

--- src/extensible-areas/user-list-dropdown-item/component.ts
+++ src/extensible-areas/user-list-dropdown-item/component.ts
@@ -1,8 +1,12 @@
 import {
   UserListDropdownInterface, UserListDropdownOptionProps,
-  UserListDropdownSeparatorProps, UserListDropdownInformationProps,
+  UserListDropdownSeparatorProps,
+  UserListDropdownTitleActionProps,
+  UserListDropdownTitleActionOnClickArguments,
+  UserListDropdownFixedContentInformationProps,
+  UserListDropdownGenericContentInformationProps,
 } from './types';
-import { UserListDropdownItemType } from './enums';
+import { UserListDropdownItemType, UserListDropdownSeparatorPosition } from './enums';
 
 // UserListDropdown Extensible Area
 
@@ -60,6 +64,8 @@ export class UserListDropdownSeparator implements UserListDropdownInterface {
 
   userId: string;
 
+  position: UserListDropdownSeparatorPosition = UserListDropdownSeparatorPosition.AFTER;
+
   type: UserListDropdownItemType;
 
   /**
@@ -71,8 +77,9 @@ export class UserListDropdownSeparator implements UserListDropdownInterface {
    *
    * @returns Object that will be interpreted by the core of Bigbluebutton (HTML5).
    */
-  constructor({ userId = '' }: UserListDropdownSeparatorProps) {
+  constructor({ userId = '', position = UserListDropdownSeparatorPosition.AFTER }: UserListDropdownSeparatorProps) {
     this.userId = userId;
+    this.position = position;
     this.type = UserListDropdownItemType.SEPARATOR;
   }
 
@@ -81,7 +88,7 @@ export class UserListDropdownSeparator implements UserListDropdownInterface {
   };
 }
 
-export class UserListDropdownInformation implements UserListDropdownInterface {
+export class UserListDropdownFixedContentInformation implements UserListDropdownInterface {
   id: string = '';
 
   userId: string;
@@ -117,17 +124,101 @@ export class UserListDropdownInformation implements UserListDropdownInterface {
   constructor({
     label = '', icon = '', iconRight = '', allowed = true,
     userId = '', textColor = '',
-  }: UserListDropdownInformationProps) {
+  }: UserListDropdownFixedContentInformationProps) {
     this.userId = userId;
     this.label = label;
     this.icon = icon;
     this.iconRight = iconRight;
     this.textColor = textColor;
     this.allowed = allowed;
-    this.type = UserListDropdownItemType.INFORMATION;
+    this.type = UserListDropdownItemType.FIXED_CONTENT_INFORMATION;
   }
 
   setItemId: (id: string) => void = (id: string) => {
-    this.id = `UserListDropdownInformation_${id}`;
+    this.id = `UserListDropdownFixedContentInformation_${id}`;
+  };
+}
+
+export class UserListDropdownGenericContentInformation
+implements UserListDropdownInterface {
+  id: string = '';
+
+  userId: string;
+
+  allowed: boolean;
+
+  type: UserListDropdownItemType;
+
+  contentFunction: (element: HTMLElement) => void;
+
+  /**
+   * Returns object to be used in the setter for the User List Dropdown. In this case,
+   * a button.
+   *
+   * @param label - label to be displayed in user list dropdown information.
+   * @param icon - icon to be used in the user list dropdown information.
+   * It goes on the left side of it.
+   * @param iconRight - icon to be used in the user list dropdown information.
+   * It goes on the right side of it.
+   * @param allowed - if false, the use list dropdown  will not appear in the dropdown.
+   * @param textColor - Color that the text will have.
+   * @param userId - the userId in which this dropdown information will appear when the user
+   * list item is clicked.
+   *
+   * @returns Object that will be interpreted by the core of Bigbluebutton (HTML5).
+   */
+  constructor({
+    contentFunction, allowed = true,
+    userId = '',
+  }: UserListDropdownGenericContentInformationProps) {
+    this.userId = userId;
+    this.allowed = allowed;
+    this.contentFunction = contentFunction;
+    this.type = UserListDropdownItemType.GENERIC_CONTENT_INFORMATION;
+  }
+
+  setItemId: (id: string) => void = (id: string) => {
+    this.id = `UserListDropdownGenericContentInformation_${id}`;
+  };
+}
+
+export class UserListDropdownTitleAction implements UserListDropdownInterface {
+  id: string = '';
+
+  userId: string;
+
+  type: UserListDropdownItemType;
+
+  icon: string;
+
+  tooltip: string;
+
+  onClick: (args: UserListDropdownTitleActionOnClickArguments) => void;
+
+  /**
+   * Returns object to be used in the setter for the User List Dropdown. In this case,
+   * a button (action) for the title.
+   *
+   * @param onClick - onClick function.
+   * @param tooltip - quick explanatory message that will appear when user hover the action.
+   * @param icon - icon to be used in the user list dropdown title action.
+   * It goes on the left side of it.
+   * @param userId - the userId in which this dropdown title action will appear when the user
+   * list item is clicked.
+   *
+   * @returns Object that will be interpreted by the core of Bigbluebutton (HTML5).
+   */
+  constructor({
+    icon = '', userId = '', onClick, tooltip,
+  }: UserListDropdownTitleActionProps) {
+    this.userId = userId;
+    this.icon = icon;
+    this.tooltip = tooltip;
+    this.onClick = onClick;
+    this.type = UserListDropdownItemType.TITLE_ACTION;
+  }
+
+  setItemId: (id: string) => void = (id: string) => {
+    this.id = `UserListDropdownTitleAction_${id}`;
   };
 }
