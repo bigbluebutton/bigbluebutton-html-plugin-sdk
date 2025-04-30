ee437f0129f3832161e5a5e2ab6bd76a62b8701f
Guilherme Leme <leme.guilherme.p@gmail.com>
2025-04-30 12:05:56 -0300

    [fix-locale-fallback] fix locale fallback not being respected.

--- src/core/auxiliary/plugin-information/locale-messages/useLocaleMessages.ts
+++ src/core/auxiliary/plugin-information/locale-messages/useLocaleMessages.ts
@@ -1,7 +1,8 @@
-import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { IntlLocaleUiDataNames } from '../../../../ui-data-hooks';
 import { pluginLogger } from '../../../../utils';
 import { IntlMessages, UseLocaleMessagesProps } from './types';
+import { fetchLocaleAndStore, mergeLocaleMessages } from './utils';
 
 function useLocaleMessagesAuxiliary(
   { pluginApi, fetchConfigs }: UseLocaleMessagesProps,
@@ -11,20 +12,35 @@ function useLocaleMessagesAuxiliary(
     fallbackLocale: 'en',
   });
 
-  const [loading, setLoading] = React.useState(true);
-  const [messages, setMessages] = React.useState<Record<string, string>>({});
+  const [loading, setLoading] = useState(true);
+  const [messages, setMessages] = useState<Record<string, string>>({});
+  const [fallbackMessages, setFallbackMessages] = useState<Record<string, string>>();
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (pluginApi?.localesBaseUrl && currentLocale.locale) {
       const { localesBaseUrl } = pluginApi;
-      const { locale } = currentLocale;
+      const { locale, fallbackLocale } = currentLocale;
       const localeUrl = `${localesBaseUrl}/${locale}.json`;
-      fetch(localeUrl, fetchConfigs).then((result) => result.json()).then((localeMessages) => {
+      const fallbackLocaleUrl = `${localesBaseUrl}/${fallbackLocale}.json`;
+      Promise.all([
+        localeUrl,
+        fallbackLocaleUrl,
+      ].map((url) => {
+        if (url !== fallbackLocaleUrl || !fallbackMessages) {
+          try {
+            return fetchLocaleAndStore(url, fetchConfigs);
+          } catch (err) {
+            pluginLogger.error(`Something went wrong while trying to fetch ${localeUrl}: `, err);
+            return Promise.reject(err);
+          }
+        }
+        return Promise.resolve(fallbackMessages);
+      })).then((values) => {
+        const [desiredLocale, fallbackLocaleMessages] = values;
+        setMessages(mergeLocaleMessages(desiredLocale, fallbackLocaleMessages));
+        if (!fallbackMessages) setFallbackMessages(fallbackLocaleMessages);
+      }).finally(() => {
         setLoading(false);
-        setMessages(localeMessages);
-      }).catch((err) => {
-        setLoading(false);
-        pluginLogger.error(`Something went wrong while trying to fetch ${localeUrl}: `, err);
       });
     }
   }, [currentLocale]);
